95eefb0a6c2a1018d9303834c7b615972c1ebdc4
Alec DuBois <after.alec@gmail.com>
2020-09-22 10:35:44 -0700

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,17 +1,19 @@
-export default function shoppingListItemReducer(state = {
-  items: []
-}, action) {
-  console.log(action)
-  switch(action.type) {
-
-    case 'INCREASE_COUNT':
-      console.log(state.items.concat(state.items.length));
-      return Object.assign({}, {
-        items: state.items.concat(state.items.length)
-      });
+export default function shoppingListItemReducer(
+  state = {
+    items: [],
+  },
+  action
+) {
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return Object.assign(
+        {},
+        {
+          items: state.items.concat(state.items.length),
+        }
+      );
 
     default:
-      console.log(state)
       return state;
   }
-};
+}
